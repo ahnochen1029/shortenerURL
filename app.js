2d13f1ae179c5346b99551d71708c1ecf96cccab
Ahno Chen <ahnochen1029@gmail.com>
2020-10-02 16:02:17 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
-const mongoose = require('mongoose')
+
 const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 
-mongoose.connect('mongodb://localhost/shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -22,7 +15,6 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-
 
 app.listen(port, () => {
   console.log(`The app is running on http://localhost:${port}`)
